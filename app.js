27ba7fe57fdb27a8427900eac5134205dcc9153d
rehiven <cvargas199516@hotmail.com>
2020-01-24 11:48:12 -0600

    implement connection with mongo and some querys on views

--- app.js
+++ app.js
@@ -6,17 +6,37 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT|| 3000;
+const nav = [{link: "/books", title: "Books"}, {link: "/authors", title: "Authors"}];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav)
 
 app.use(morgan('tiny')); //show information about browser request 
+app.use((req, res, next)=>{
+  debug('my middleware');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css',express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js',express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js',express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
-//app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
+
+
+app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
+
 app.get('/', (req, res)=>{
-  res.render('index', {list: ["a", "b", "c"], title: "Library"});
+  res.render(
+    'index', 
+      {
+        nav, 
+        title: "Library"
+      }
+    );
 });
 
 app.listen(port, ()=>{
